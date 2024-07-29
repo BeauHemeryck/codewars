81c785aa33cb8c91b9c9f9c05b7a735dcb3017dc
BeauHemeryck <HemeryckBeau1999@gmail.com>
2024-07-29 21:51:03 +0200

    didn't get program to work

---
+++ 5 kyu/First non-repeating character.js
@@ -0,0 +1,11 @@
+function firstNonRepeatingLetter(s) {
+    s = s.split("");
+    if (s.length == 1) {
+        return s
+    }
+    
+    
+    return "";
+}
+
+console.log(firstNonRepeatingLetter("abba"));
