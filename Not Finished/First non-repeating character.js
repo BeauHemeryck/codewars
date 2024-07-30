1676fc2639dfa756b5323f56a94442f2752fefe9
BeauHemeryck <HemeryckBeau1999@gmail.com>
2024-07-30 03:37:53 +0200

    added exercise to correct folder

---
+++ Not Finished/First non-repeating character.js
@@ -0,0 +1,10 @@
+function firstNonRepeatingLetter(s) {
+    s = s.split("");
+    if (s.length == 1) {
+        return s
+    }
+    
+    return "";
+}
+
+console.log(firstNonRepeatingLetter("abba"));
