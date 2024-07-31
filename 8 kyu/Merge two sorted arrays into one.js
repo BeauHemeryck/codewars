1478b8892ee9a6ca09d271923204a241d214dd23
BeauHemeryck <HemeryckBeau1999@gmail.com>
2024-07-31 21:43:15 +0200

    Completed exercise

--- 8 kyu/Merge two sorted arrays into one.js
+++ 8 kyu/Merge two sorted arrays into one.js
@@ -1,8 +1,6 @@
 function mergeArrays(arr1, arr2) {
     let mergedArray = arr1.concat(arr2);
-
     let noDuplicatesArray = [...new Set(mergedArray)]
-
 
     return noDuplicatesArray.sort((a, b) => a - b);
 }
