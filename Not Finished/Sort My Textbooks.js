89bf6d441fdef2de5cd8506a6970eb241ea9ee8e
BeauHemeryck <HemeryckBeau1999@gmail.com>
2024-08-01 12:17:42 +0200

    Attempted exercise but got stuck on special characters

--- Not Finished/Sort My Textbooks.js
+++ Not Finished/Sort My Textbooks.js
@@ -1,5 +1,2 @@
 function sorter(textbooks) {
-    return textbooks.map(textbook => textbook.toLowerCase()).sort()
   };
-
-console.log(sorter(['caCcdebA','Bbde','bbAebceede','aADbeadED','Ebb','bEbC','eeeebceaaa','BCdcba']))
