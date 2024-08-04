ca51be00bef5d149feaced6c3f09cbc7b4846c6f
BeauHemeryck <HemeryckBeau1999@gmail.com>
2024-08-04 16:42:48 +0200

    Removed console.log and changed < 101 to <= 100

--- 7 kyu/Find the Missing Number.js
+++ 7 kyu/Find the Missing Number.js
@@ -1,6 +1,5 @@
 function missingNo(nums) {
-    for (let i = 0; i < 101; i++) {
-        console.log(i)
+    for (let i = 0; i <= 100; i++) {
         if (!nums.includes(i)) {
             return i
         }
