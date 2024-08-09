f831fab501c660de7e6d99f0aed811d96a024829
BeauHemeryck <HemeryckBeau1999@gmail.com>
2024-08-09 02:21:48 +0200

    Completed exercise

---
+++ 6 kyu/GA-DE-RY-PO-LU-KI cypher vol 2.js
@@ -0,0 +1,49 @@
+function encode(str, key) {
+    let keyCollection = {};
+
+    for (let i = 0; i < key.length; i += 2) {
+        if (!keyCollection[key[i]]) {
+            keyCollection[key[i + 1].toLowerCase()] = key[i].toLowerCase();
+            keyCollection[key[i + 1].toUpperCase()] = key[i].toUpperCase();
+            keyCollection[key[i].toLowerCase()] = key[i + 1].toLowerCase();
+            keyCollection[key[i].toUpperCase()] = key[i + 1].toUpperCase();
+        }
+    }
+
+    return str
+        .split("")
+        .map((char) => {
+            if (keyCollection[char]) {
+                return keyCollection[char];
+            } else {
+                return char;
+            }
+        })
+        .join("");
+}
+
+function decode(str, key) {
+    let keyCollection = {};
+
+    for (let i = 0; i < key.length; i += 2) {
+        if (!keyCollection[key[i]]) {
+            keyCollection[key[i + 1].toLowerCase()] = key[i].toLowerCase();
+            keyCollection[key[i + 1].toUpperCase()] = key[i].toUpperCase();
+            keyCollection[key[i].toLowerCase()] = key[i + 1].toLowerCase();
+            keyCollection[key[i].toUpperCase()] = key[i + 1].toUpperCase();
+        }
+    }
+
+    return str
+        .split("")
+        .map((char) => {
+            if (keyCollection[char]) {
+                return keyCollection[char];
+            } else {
+                return char;
+            }
+        })
+        .join("");
+}
+
+console.log(encode("Ala has a cat", "gaderypoluki"));
